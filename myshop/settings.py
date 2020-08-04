62677b6dfb1f61e55901b17ce5cbb98f32a2a551
jessc1 <mmariajessica@gmail.com>
2020-08-03 23:28:04 -0300

    Exporting orders to CSV files

--- myshop/settings.py
+++ myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payment.apps.PaymentConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,20 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID= 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Braintree settings
+BRAINTREE_MERCHANT_ID = 'knst3fgqftxcfxxm'  # Merchant ID
+BRAINTREE_PUBLIC_KEY = 'zkdvjy3w9c3y6pj9'   # Public Key
+BRAINTREE_PRIVATE_KEY = 'f40656b2e064c7fe4b8f8b991a6f1a62'  # Private key
+
+import braintree
+
+BRAINTREE_CONF = braintree.Configuration(
+    braintree.Environment.Sandbox,
+    BRAINTREE_MERCHANT_ID,
+    BRAINTREE_PUBLIC_KEY,
+    BRAINTREE_PRIVATE_KEY
+)
+
+
+
